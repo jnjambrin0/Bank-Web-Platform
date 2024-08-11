90c863f82d1d7f3f9495559db0813103095ddbfe
jnjambrin0 <125131418+jnjambrin0@users.noreply.github.com>
2024-08-11 14:12:56 +0200

    chore: Refactor Sidebar component to include user and footer sections

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -13,7 +13,7 @@ const Sidebar = ({ user }: SiderbarProps) => {
   return (
     <section className="sidebar">
       <nav className="flex flex-col gap-4">
-        <Link href="/" className="mb-12 cursor-pointer items-center gap-2">
+        <Link href="/" className="flex mb-12 cursor-pointer items-center gap-2">
           <Image
             src="/icons/logo.svg"
             width={32}
@@ -23,7 +23,6 @@ const Sidebar = ({ user }: SiderbarProps) => {
           />
           <h1 className="sidebar-logo">Horizon</h1>
         </Link>
-
         {sidebarLinks.map((item) => {
           const isActive =
             pathName === item.route || pathName.startsWith(`${item.route}/`);
@@ -34,12 +33,22 @@ const Sidebar = ({ user }: SiderbarProps) => {
               className={cn("sidebar-link", { "bg-bank-gradient": isActive })}
             >
               <div className="relative size-6">
-                <Image src={item.imgURL} alt={item.label} fill />
+                <Image
+                  src={item.imgURL}
+                  alt={item.label}
+                  fill
+                  className={cn({ "brightness-[3] invert-0": isActive })}
+                />
               </div>
+              <p className={cn("sidebar-label", { "!text-white": isActive })}>
+                {item.label}
+              </p>
             </Link>
           );
         })}
+        USER
       </nav>
+      FOOTER
     </section>
   );
 };
