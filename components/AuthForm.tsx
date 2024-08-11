8ff5454e24f395be67a041e855c6b13f91ff5b7e
jnjambrin0 <125131418+jnjambrin0@users.noreply.github.com>
2024-08-11 16:50:52 +0200

    feat: Add @hookform/resolvers, @radix-ui/react-label, and @radix-ui/react-slot dependencies

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -1,7 +1,41 @@
-import React from "react";
+"use client";
+
+import Image from "next/image";
+import Link from "next/link";
+import React, { useState } from "react";
 
 const AuthForm = ({ type }: { type: string }) => {
-  return <div>{type}</div>;
+  const [user, setUser] = useState(null);
+
+  return (
+    <section className="auth-form">
+      <header className="flex flex-col gap-5 md:gap-8">
+        <Link href="/" className="flex cursor-pointer items-center gap-1">
+          <Image
+            src="/icons/logo.svg"
+            width={32}
+            height={32}
+            alt="HorizonLogo"
+          />
+          <h1 className="text-26 font-ibm-plex-serif font-bold text-black-1">
+            Horizon
+          </h1>
+        </Link>
+
+        <div className="flex flex-col gap-1 md:gap-3">
+          <h1 className="text-24 lg:text-36 font-semibold text-gray-900">
+            {user ? "Link Account" : type === "sign-in" ? "Sign In" : "Sign Up"}
+            <p className="text-16 font-normal text-gray-600">
+              {user
+                ? "Link your account to get started"
+                : "Please enter your details"}
+            </p>
+          </h1>
+        </div>
+      </header>
+      {user ? <div className="flex flex-col gap-4">{}</div> : <>FORM</>}
+    </section>
+  );
 };
 
 export default AuthForm;
