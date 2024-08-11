67625c1866196fa8d2a915d0bb47ba0dffcd6be5
jnjambrin0 <125131418+jnjambrin0@users.noreply.github.com>
2024-08-11 22:08:26 +0200

    refactor: Remove unnecessary code and improve sign-up page

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -39,13 +39,13 @@ const AuthForm = ({ type }: { type: string }) => {
         setUser(newUser);
       }
       if (type === "sign-in") {
-        /*const response = await signIn({
+        const response = await signIn({
           email: data.email,
           password: data.password,
         });
         if (response) {
           router.push("/");
-        }*/
+        }
       }
     } catch (error) {
       console.error(error);
