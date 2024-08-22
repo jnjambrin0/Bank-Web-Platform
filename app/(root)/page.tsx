23ffe64afba06c1ecc5a1660ce44ec30871800dc
jnjambrin0 <125131418+jnjambrin0@users.noreply.github.com>
2024-08-22 13:47:15 +0200

    feat: Add getBanks and getBank functions to user.actions for bank retrieval

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,11 +1,21 @@
 import HeaderBox from "@/components/HeaderBox";
 import RightSidebar from "@/components/RightSidebar";
 import TotalBalanceBox from "@/components/TotalBalanceBox";
+import { getAccount, getAccounts } from "@/lib/actions/bank.actions";
 import { getLoggedInUser } from "@/lib/actions/user.actions";
 import React from "react";
 
-const Home = async () => {
+const Home = async ({ searchParams: { id, page } }: SearchParamProps) => {
   const loggedIn = await getLoggedInUser();
+  const accounts = await getAccounts({ userId: loggedIn.$id });
+
+  if (!accounts) return <div>Loading...</div>;
+
+  const accountsData = accounts?.data || [];
+  const appwriteItemId = (id as string) || accountsData[0]?.appwriteItemId;
+
+  const account = await getAccount({ appwriteItemId });
+
   return (
     <section className="home">
       <div className="home-content">
@@ -17,17 +27,17 @@ const Home = async () => {
             subtext="Access and manage your account and transactions efficiently"
           />
           <TotalBalanceBox
-            accounts={[]}
-            totalBanks={1}
-            totalCurrentBalance={21532.25}
+            accounts={accountsData}
+            totalBanks={accounts?.totalBanks || 0}
+            totalCurrentBalance={accounts?.totalCurrentBalance || 0}
           />
         </header>
         RECENT TRANSACTIONS
       </div>
       <RightSidebar
         user={loggedIn}
-        transactions={[]}
-        banks={[{ currentBalance: 123.5 }, { currentBalance: 500.0 }]}
+        transactions={account?.transactions || []}
+        banks={accountsData?.slice(0, 2) || []}
       />
     </section>
   );
