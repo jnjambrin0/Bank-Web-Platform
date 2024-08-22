1967f7ceb91e1225d6599b1405478639b088afe4
jnjambrin0 <125131418+jnjambrin0@users.noreply.github.com>
2024-08-22 17:29:12 +0200

    feat: Add RecentTransactions component to Home page for displaying recent transactions

--- components/DoughnutChart.tsx
+++ components/DoughnutChart.tsx
@@ -6,6 +6,9 @@ import { Doughnut } from "react-chartjs-2";
 ChartJS.register(ArcElement, Tooltip, Legend);
 
 const DoughnutChart = ({ accounts }: DoughnutChartProps) => {
+  const accountNames = accounts.map((a) => a.name);
+  const accountBalances = accounts.map((a) => a.currentBalance);
+
   const data = {
     datasets: [
       {
