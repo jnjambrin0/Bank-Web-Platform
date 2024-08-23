99e08d0212801faef4df56a576813e3c0d9a0c09
jnjambrin0 <125131418+jnjambrin0@users.noreply.github.com>
2024-08-23 13:48:17 +0200

    refactor: Update MyBanks page to include dynamic loading of bank accounts and display bank cards

--- components/BankCard.tsx
+++ components/BankCard.tsx
@@ -2,15 +2,20 @@ import { formatAmount } from "@/lib/utils";
 import Image from "next/image";
 import Link from "next/link";
 import React, { use } from "react";
+import Copy from "./Copy";
 
 const BankCard = ({
   account,
   userName,
   showBalance = true,
 }: CreditCardProps) => {
+  console.log("Account: ", account);
   return (
     <div className="flex flex-col">
-      <Link href={`/`} className="bank-card">
+      <Link
+        href={`/transaction-history/?id=${account.appwriteItemId}`}
+        className="bank-card"
+      >
         <div className="bank-card_content">
           <div>
             <h1 className="text-16 font-semibold text-white">{userName}</h1>
@@ -25,7 +30,9 @@ const BankCard = ({
             </div>
             <p className="text-14 font-semibold tracking-[1.1px] text-white">
               ●●●● ●●●● ●●●●{" "}
-              <span className="text-16">{account.mask ? "1234" : "1234"}</span>
+              <span className="text-16">
+                {account.mask ? account.mask : "1234"}
+              </span>
             </p>
           </article>
         </div>
@@ -48,6 +55,8 @@ const BankCard = ({
           className="absolute top-0 left-0"
         />
       </Link>
+
+      {showBalance && <Copy title={account?.shareableId} />}
     </div>
   );
 };
