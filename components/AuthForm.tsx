cbc48e0729f137ba26e3e6aa3ecda5e8496256c9
jnjambrin0 <125131418+jnjambrin0@users.noreply.github.com>
2024-08-27 16:47:13 +0200

    refactor: Add error and success notifications to AuthForm component

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -19,6 +19,8 @@ const AuthForm = ({ type }: { type: string }) => {
   const router = useRouter();
   const [user, setUser] = useState(null);
   const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(false);
+  const [success, setSuccess] = useState(false);
 
   const formSchema = authFormSchema(type);
 
@@ -33,6 +35,8 @@ const AuthForm = ({ type }: { type: string }) => {
 
   // 2. Define a submit handler.
   const onSubmit = async (data: z.infer<typeof formSchema>) => {
+    setError(false);
+    setSuccess(false);
     setIsLoading(true);
     try {
       if (type === "sign-up") {
@@ -51,7 +55,13 @@ const AuthForm = ({ type }: { type: string }) => {
 
         const newUser = await signUp(userData);
 
-        setUser(newUser);
+        if (!newUser) {
+          setError(true);
+        } else {
+          setSuccess(true);
+
+          setUser(newUser);
+        }
       }
 
       if (type === "sign-in") {
@@ -59,6 +69,11 @@ const AuthForm = ({ type }: { type: string }) => {
           email: data.email,
           password: data.password,
         });
+
+        if (!response) setError(true);
+        else {
+          setSuccess(true);
+        }
 
         if (response) {
           router.push("/");
@@ -97,6 +112,40 @@ const AuthForm = ({ type }: { type: string }) => {
           </h1>
         </div>
       </header>
+      {success && (
+        <div
+          className="animate-fadeIn bg-green-100 border text-green-700 px-4 py-3 rounded-lg shadow-lg flex items-center"
+          role="alert"
+        >
+          <svg
+            className="fill-current w-6 h-6 mr-4"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 20 20"
+          >
+            <path d="M9 12l2 2 4-4-1.41-1.42L11 11.17l-1.59-1.58L8 11l1 1z" />
+          </svg>
+          <div>
+            <p className="font-bold">¡Operación exitosa!</p>
+            <p className="text-sm">
+              {type === "sign-in" ? "Has iniciado sesión" : "Te has registrado"}{" "}
+              correctamente.
+            </p>
+          </div>
+        </div>
+      )}
+      {error && (
+        <div
+          className="animate-fadeIn bg-red-100 border text-red-700 px-4 py-3 rounded-lg shadow-lg flex items-center"
+          role="alert"
+        >
+          <div>
+            <p className="font-bold">Error: </p>
+            <p className="text-sm">
+              Verifica tus credenciales e inténtalo de nuevo.
+            </p>
+          </div>
+        </div>
+      )}
       {user ? (
         <div className="flex flex-col gap-4">
           <PlaidLink user={user} variant="primary" />
